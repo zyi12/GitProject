29435cec29464d2c4b4121f97a4a7caf9e76c091
Zyill Alcarez <zalcarez.gotechsolutions@gmail.com>
2018-07-24 09:14:10 +0800

    initial files

---
+++ src/GitMain.java
@@ -0,0 +1,16 @@
+
+public class GitMain {
+
+	public static void main(String[] args) {
+		System.err.println("1");
+		System.err.println("2");
+		System.err.println("3");
+		System.err.println("4");
+		System.err.println("5");
+		System.err.println("6");
+		System.err.println("7");
+		System.err.println("8");
+		System.err.println("9");
+	}
+
+}
